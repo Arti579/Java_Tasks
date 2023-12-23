e64f8e929a41e3445f6700c29e8d1c1adc37274f
Arti579 <135072509+Arti579@users.noreply.github.com>
2023-12-23 21:50:00 +0300

    Delete Task_2 directory

--- Task_2/Ball/TestBall.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-public class TestBall {
-    public static void main (String[] args) {
-        Ball ball = new Ball();
-        System.out.println("X: "+ ball.getX()+"; Y: "+ball.getY()+";");
-        ball.move(0.5, -0.5);
-        System.out.println(ball.toString());
-    }
-}
