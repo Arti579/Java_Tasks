fd91012d3c767850c376ec2b631bd5091e5ff1a4
Arti579 <135072509+Arti579@users.noreply.github.com>
2023-12-23 22:25:47 +0300

    Add files via upload

---
+++ Task_2/Task_2_2/Mai.java
@@ -0,0 +1,13 @@
+public class Mai {
+    public static void main(String[] args) {
+        LabradorRetriever labrador = new LabradorRetriever();
+        System.out.println("Breed: " + labrador.getBreed());
+        labrador.bark();
+
+        System.out.println();
+
+        GermanShepherd germanShepherd = new GermanShepherd();
+        System.out.println("Breed: " + germanShepherd.getBreed());
+        germanShepherd.bark();
+    }
+}
