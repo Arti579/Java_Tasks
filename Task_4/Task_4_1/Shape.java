b54d18a9535278fb4148880954d78017b1e0d626
Arti579 <135072509+Arti579@users.noreply.github.com>
2023-12-23 22:07:28 +0300

    Delete Task_4 directory

--- Task_4/Task_4_1/Shape.java
+++
@@ -1,5 +0,0 @@
-public abstract class Shape {
-    protected double Pi_number = Math.PI;
-    public abstract double calculateArea();
-    public abstract double calculatePerimeter();
-}
--- Task_4/Task_4_3/Movable.java
+++
@@ -1,7 +0,0 @@
-interface Movable {
-    public void moveUp();
-    public void moveDown();
-    public void moveLeft();
-    public void moveRight();
-
-}
