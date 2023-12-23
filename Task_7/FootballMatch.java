0c4c695839341adc1c51f096576844fe974807bd
Arti579 <135072509+Arti579@users.noreply.github.com>
2023-12-23 23:23:43 +0300

    Add files via upload

---
+++ Task_7/FootballMatch.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FootballMatch extends JFrame {
+    private int milanScore = 0;
+    private int madridScore = 0;
+
+    private JLabel resultLabel;
+    private JLabel lastScorerLabel;
+    private JLabel winnerLabel;
+
+    public FootballMatch() {
+        setTitle("Football Match");
+        setSize(300, 170);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JButton milanButton = new JButton("AC Milan");
+        JButton madridButton = new JButton("Real Madrid");
+
+        resultLabel = new JLabel("Result: " + milanScore + " X " + madridScore);
+        lastScorerLabel = new JLabel("Last Scorer: N/A");
+        winnerLabel = new JLabel("Winner: DRAW");
+
+        milanButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                milanScore++;
+                updateLabels("AC Milan");
+            }
+        });
+
+        madridButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                madridScore++;
+                updateLabels("Real Madrid");
+            }
+        });
+
+        setLayout(new GridLayout(3, 2));
+        add(milanButton);
+        add(madridButton);
+        add(resultLabel);
+        add(lastScorerLabel);
+        add(winnerLabel);
+    }
+
+    private void updateLabels(String lastScorer) {
+        resultLabel.setText("Result: " + milanScore + " X " + madridScore);
+        lastScorerLabel.setText("Last Scorer: " + lastScorer);
+
+        if (milanScore > madridScore) {
+            winnerLabel.setText("Winner: AC Milan");
+        } else if (madridScore > milanScore) {
+            winnerLabel.setText("Winner: Real Madrid");
+        } else {
+            winnerLabel.setText("Winner: DRAW");
+        }
+    }
+
+    public static void main(String[] args) {
+        FootballMatch match = new FootballMatch();
+        match.setVisible(true);
+    }
+}
